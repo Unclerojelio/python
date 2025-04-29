3d94dada519cb2453073614a6892d6f563a7435b
Roger Banks <roger_banks@mac.com>
2025-04-29 10:44:11 -0500

    Update MyCaches.py
    Update to py3, new namespace, format found date.

--- MyCaches/MyCaches.py
+++ MyCaches/MyCaches.py
@@ -9,31 +9,33 @@ count = 0
 my_caches = []
 tree = ET.parse('8255490.gpx')
 root = tree.getroot()
-print root.tag, root.attrib
+print(root.tag, root.attrib)
 for wpt in root.findall('{http://www.topografix.com/GPX/1/0}wpt'):
     gc_num = wpt.find('{http://www.topografix.com/GPX/1/0}name').text
     desc = wpt.find('{http://www.topografix.com/GPX/1/0}desc').text
     time = wpt.find('{http://www.topografix.com/GPX/1/0}time').text.split('T')
     date_str = time[0].split('-')
-    find_date = date(int(date_str[0]), int(date_str[1]), int(date_str[2]))
-    cache = wpt.find('{http://www.groundspeak.com/cache/1/0}cache')
-    container = cache.find('{http://www.groundspeak.com/cache/1/0}container').text
-    name = cache.find('{http://www.groundspeak.com/cache/1/0}name').text
-    owner = cache.find('{http://www.groundspeak.com/cache/1/0}owner').text
-    type = cache.find('{http://www.groundspeak.com/cache/1/0}type').text
-    difficulty = cache.find('{http://www.groundspeak.com/cache/1/0}difficulty').text
-    terrain = cache.find('{http://www.groundspeak.com/cache/1/0}terrain').text
-    logs = cache.find('{http://www.groundspeak.com/cache/1/0}logs')
-    log = logs.find('{http://www.groundspeak.com/cache/1/0}log')
-    found_date = log.find('{http://www.groundspeak.com/cache/1/0}date').text
+    placed_date = date(int(date_str[0]), int(date_str[1]), int(date_str[2]))
+    cache = wpt.find('{http://www.groundspeak.com/cache/1/0/1}cache')
+    container = cache.find('{http://www.groundspeak.com/cache/1/0/1}container').text
+    name = cache.find('{http://www.groundspeak.com/cache/1/0/1}name').text
+    owner = cache.find('{http://www.groundspeak.com/cache/1/0/1}owner').text
+    type = cache.find('{http://www.groundspeak.com/cache/1/0/1}type').text
+    difficulty = cache.find('{http://www.groundspeak.com/cache/1/0/1}difficulty').text
+    terrain = cache.find('{http://www.groundspeak.com/cache/1/0/1}terrain').text
+    logs = cache.find('{http://www.groundspeak.com/cache/1/0/1}logs')
+    log = logs.find('{http://www.groundspeak.com/cache/1/0/1}log')
+    found_date_str = log.find('{http://www.groundspeak.com/cache/1/0/1}date').text.split('T')
+    found_date_str = found_date_str[0].split('-')
+    found_date = date(int(found_date_str[0]), int(found_date_str[1]), int(found_date_str[2]))
     format_dt = '(' + difficulty + '/' + terrain + ')'
-#    print name, desc, find_date, time[1], wpt.attrib['lat'], wpt.attrib['lon'], container, found_date
+#    print(name, desc, find_date, time[1], wpt.attrib['lat'], wpt.attrib['lon'], container, found_date)
 #    print found_date, gc_num, name, owner, type, terrain, difficulty, container
-    my_caches.append([found_date, gc_num, name, owner, type, format_dt, container])
+    my_caches.append([found_date.strftime('%Y-%m-%d'), gc_num, name, owner, type, format_dt, container])
     count += 1
 my_caches.sort()
 for cache in my_caches:
-     print cache
-print count
+    print(cache)
+print(count)
 
 # print sorted list of found caches [(date_found, gc_num, name, owner, type, container, lat, lon)]
